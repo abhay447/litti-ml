5c7992c6b9cd5cea7c0ffd199ced235979014716
abhay447 <abhay447@gmail.com>
2023-02-25 22:38:09 +0530

    added dream11 model

--- src/main/java/com/litti/ml/runtime/Main.java
+++ src/main/java/com/litti/ml/runtime/Main.java
@@ -12,18 +12,27 @@ import java.nio.file.Paths;
 public class Main {
 
   public static void main(String[] args) throws IOException {
-    ModelMetadata modelMetadata = null;
+    ModelMetadata sampleModelMetadata = null;
+    ModelMetadata dream11ModelMetadata = null;
     ObjectMapper objectMapper = JsonMapper.builder().addModule(new GuavaModule()).build();
     try {
       final String modelJson =
           Files.readString(Paths.get(Resources.getResource("sample-model-meta.json").toURI()));
-      modelMetadata = objectMapper.readValue(modelJson, ModelMetadata.class);
+      sampleModelMetadata = objectMapper.readValue(modelJson, ModelMetadata.class);
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+    try {
+      final String modelJson =
+              Files.readString(Paths.get(Resources.getResource("dream11-model-meta.json").toURI()));
+      dream11ModelMetadata = objectMapper.readValue(modelJson, ModelMetadata.class);
     } catch (Exception e) {
       e.printStackTrace();
     }
 
     final ModelRegistry modelRegistry = new ModelRegistry();
-    modelRegistry.addModelForPrediction(modelMetadata);
+    modelRegistry.addModelForPrediction(sampleModelMetadata);
+    modelRegistry.addModelForPrediction(dream11ModelMetadata);
 
     RuntimeHTTPServer runtimeHTTPServer = new RuntimeHTTPServer(modelRegistry);
   }
