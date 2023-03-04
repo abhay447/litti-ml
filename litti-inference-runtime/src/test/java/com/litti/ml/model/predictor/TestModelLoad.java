bc5afc7856bd5fbaaa4ed88281bc894d7fe6fbb3
abhay447 <abhay447@gmail.com>
2023-03-04 10:31:13 +0530

    moved to junit5 and added model load test

---
+++ litti-inference-runtime/src/test/java/com/litti/ml/model/predictor/TestModelLoad.java
@@ -0,0 +1,26 @@
+package com.litti.ml.model.predictor;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.json.JsonMapper;
+import com.fasterxml.jackson.datatype.guava.GuavaModule;
+import com.litti.ml.model.entities.ModelMetadata;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import org.apache.curator.shaded.com.google.common.io.Resources;
+import org.junit.jupiter.api.Test;
+
+public class TestModelLoad {
+
+  final ObjectMapper objectMapper = JsonMapper.builder().addModule(new GuavaModule()).build();
+
+  @Test
+  public void testPmmlLoad() throws Exception {
+    String json =
+        Files.readString(
+            Paths.get(Resources.getResource("model-metadata/dream11-model-meta.json").toURI()));
+    final ModelMetadata modelMetadata = objectMapper.readValue(json, ModelMetadata.class);
+    assertDoesNotThrow(() -> new PMMLPredictor(modelMetadata, null));
+  }
+}
