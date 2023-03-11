8e139427846ad5c2496a95a552d9db6b3dab0ffe
abhay447 <abhay447@gmail.com>
2023-03-11 22:30:57 +0530

    added sample springboot controller

--- litti-management-server/src/main/java/com/litti/ml/management/Application.java
+++ litti-management-server/src/main/java/com/litti/ml/management/Application.java
@@ -1,6 +1,5 @@
 package com.litti.ml.management;
 
-import java.util.Arrays;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -16,14 +15,6 @@ public class Application {
 
   @Bean
   public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
-    return args -> {
-      System.out.println("Let's inspect the beans provided by Spring Boot:");
-
-      String[] beanNames = ctx.getBeanDefinitionNames();
-      Arrays.sort(beanNames);
-      for (String beanName : beanNames) {
-        System.out.println(beanName);
-      }
-    };
+    return args -> {};
   }
 }
