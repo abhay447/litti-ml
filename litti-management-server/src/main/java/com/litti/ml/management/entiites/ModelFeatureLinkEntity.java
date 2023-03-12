946d8397894b2bda54ec5004baa480a78ecf0d31
abhay447 <abhay447@gmail.com>
2023-03-12 12:45:12 +0530

    added model feature link entity and repository

---
+++ litti-management-server/src/main/java/com/litti/ml/management/entiites/ModelFeatureLinkEntity.java
@@ -0,0 +1,41 @@
+package com.litti.ml.management.entiites;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+import java.util.UUID;
+
+@Entity
+public class ModelFeatureLinkEntity {
+
+  @Id @GeneratedValue private UUID id;
+  private UUID modelId;
+  private UUID featureId;
+
+  public ModelFeatureLinkEntity() {}
+
+  public UUID getId() {
+    return id;
+  }
+
+  public void setId(UUID id) {
+    this.id = id;
+  }
+
+  public UUID getModelId() {
+    return modelId;
+  }
+
+  public void setModelId(UUID modelId) {
+    this.modelId = modelId;
+  }
+
+  public UUID getFeatureId() {
+    return featureId;
+  }
+
+  public void setFeatureId(UUID featureId) {
+    this.featureId = featureId;
+  }
+}
