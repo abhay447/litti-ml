04ce713cb25816d2528a8acd410421564f4e5976
abhay447 <abhay447@gmail.com>
2023-08-19 00:18:03 +0530

    make sidebar clickable

--- litti-management-ui/src/app/components/container/layout.tsx
+++ litti-management-ui/src/app/components/container/layout.tsx
@@ -1,19 +1,21 @@
 import { Col, Row } from "reactstrap"
-import SidebarProducts from "../sidebar/products/page"
+import SidebarProducts, { NAV_OPTION_MODELS } from "../sidebar/products/page"
 import "@/app/styles/entity.css"
 import "@/app/styles/sidebar.css"
+import { useState } from "react"
 
 export default function RootContainerLayout({
     children, // will be a page or nested layout
   }: {
     children: React.ReactNode
   }) {
+    let [navOption, setNavOption] = useState(NAV_OPTION_MODELS);
     return (
       <section>
         {/* Include shared UI here e.g. a header or sidebar */}
         <nav></nav>
         <Row>
-          <Col className="sidebar"><SidebarProducts/></Col>
+          <Col className="sidebar"><SidebarProducts navOption={navOption} setNavOption={setNavOption}/></Col>
           <Col xs={10} className="entity-base">
             {children}
           </Col>
--- litti-management-ui/src/app/components/sidebar/products/page.tsx
+++ litti-management-ui/src/app/components/sidebar/products/page.tsx
@@ -4,24 +4,50 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import '@/app/styles/sidebar.css'
 
 // `app/dashboard/page.tsx` is the UI for the `/dashboard` URL
-export default function SidebarProducts() { 
+export const NAV_OPTION_MODELS = "Models";
+export const NAV_OPTION_FEATURES = "Features";
+export const NAV_OPTION_MODEL_CLUSTERS = "Model Clusters";
+export const NAV_OPTION_FEATURE_STORES = "Feature Stores";
+
+export default function SidebarProducts(props: any) {
+  const navOption = props.navOption;
+  const setNavOption = props.setNavOption;
+
+  function getOptionStyle(option: string) {
+    if (option === navOption) {
+      return "sidebar-item sidebar-item-selected";
+    }
+    return "sidebar-item";
+  }
+
+  function handleOnClick(option: string) {
+    setNavOption(option);
+  }
+
   return (
-    
     <Container className="sidebar vh-100 d-flex flex-column">
       <Row className="sidebar-app-title">
         Litti Management
       </Row>
       <Row>
-        Models
+        <button type="button" className={getOptionStyle(NAV_OPTION_MODELS)} onClick={() => handleOnClick(NAV_OPTION_MODELS)}>
+          {NAV_OPTION_MODELS}
+        </button>
       </Row>
       <Row>
-        Features
+        <button type="button" className={getOptionStyle(NAV_OPTION_FEATURES)} onClick={() => handleOnClick(NAV_OPTION_FEATURES)}>
+          {NAV_OPTION_FEATURES}
+        </button>
       </Row>
       <Row>
-        Model Clusters
+        <button type="button" className={getOptionStyle(NAV_OPTION_MODEL_CLUSTERS)} onClick={() => handleOnClick(NAV_OPTION_MODEL_CLUSTERS)}>
+          {NAV_OPTION_MODEL_CLUSTERS}
+        </button>
       </Row>
       <Row>
-        Feature Stores
+        <button type="button" className={getOptionStyle(NAV_OPTION_FEATURE_STORES)} onClick={() => handleOnClick(NAV_OPTION_FEATURE_STORES)}>
+          {NAV_OPTION_FEATURE_STORES}
+        </button>
       </Row>
     </Container>
   )
