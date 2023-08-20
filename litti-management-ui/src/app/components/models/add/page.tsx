eb52c502ebe592a4467f02195deaefa71aaf9d4b
abhay447 <abhay447@gmail.com>
2023-08-20 19:29:38 +0530

    move addModel to service

--- litti-management-ui/src/app/components/models/add/page.tsx
+++ litti-management-ui/src/app/components/models/add/page.tsx
@@ -5,6 +5,7 @@ import '@/app/globals.css'
 import {ModelEntity} from '@/app/entities/modelEntity';
 import "@/app/styles/entity.css"
 import { OPTION_LIST } from "@/app/components/common/menu/page";
+import { addModel } from "@/app/services/modelService";
 
 const handleSubmit = async (event:any, setSelectedOption: any) => {
   // Stop the form from submitting and refreshing the page.
@@ -20,36 +21,7 @@ const handleSubmit = async (event:any, setSelectedOption: any) => {
     (event.target.modelOutputs as HTMLInputElement).value
   );
 
-  const data = {
-    modelEntity: modelEntity,
-    featureIds: []
-  }
-
-  // Send the data to the server in JSON format.
-  const JSONdata = JSON.stringify(data)
-
-  // API endpoint where we send form data.
-  const endpoint = 'http://localhost:8081/models'
-
-  // Form the request for sending data to the server.
-  const options = {
-    // The method is POST because we are sending data.
-    method: 'POST',
-    // Tell the server we're sending JSON.
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    // Body of the request is the JSON data we created above.
-    body: JSONdata,
-  }
-
-  // Send the form data to our forms API on Vercel and get a response.
-  const response = await fetch(endpoint, options)
-
-  // Get the response data from server as JSON.
-  // If server returns the name submitted, that means the form works.
-  const result = JSON.stringify(await response.json())
-  console.log(result);
+  await addModel(modelEntity)
   setSelectedOption(OPTION_LIST);
 }
 
--- litti-management-ui/src/app/services/modelService.tsx
+++ litti-management-ui/src/app/services/modelService.tsx
@@ -16,3 +16,36 @@ export  async function getModels(){
   );
   return models;
 }
+
+export async function addModel(modelEntity:ModelEntity){
+  const data = {
+    modelEntity: modelEntity,
+    featureIds: []
+  }
+
+  // Send the data to the server in JSON format.
+  const JSONdata = JSON.stringify(data)
+
+  // API endpoint where we send form data.
+  const endpoint = 'http://localhost:8081/models'
+
+  // Form the request for sending data to the server.
+  const options = {
+    // The method is POST because we are sending data.
+    method: 'POST',
+    // Tell the server we're sending JSON.
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    // Body of the request is the JSON data we created above.
+    body: JSONdata,
+  }
+
+  // Send the form data to our forms API on Vercel and get a response.
+  const response = await fetch(endpoint, options)
+
+  // Get the response data from server as JSON.
+  // If server returns the name submitted, that means the form works.
+  const result = JSON.stringify(await response.json())
+  console.log(result);
+}
