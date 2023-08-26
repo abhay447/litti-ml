b15efd7cd0c9819f4e900fbcada396586fa77f27
abhay447 <abhay447@gmail.com>
2023-08-27 00:54:50 +0530

    remove router calls

--- litti-management-ui/src/pages/components/feature-groups/container/page.tsx
+++ litti-management-ui/src/pages/components/feature-groups/container/page.tsx
@@ -3,7 +3,7 @@ import { Row } from "reactstrap";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '@/app/globals.css'
 import "@/app/styles/entity.css"
-import { useRouter } from "next/navigation";
+
 import EntityMenu from "@/pages/components/common/menu/page";
 import { useState } from "react";
 import FeatureGroupsListComponent from "@/pages/components/feature-groups/list/page";
@@ -12,7 +12,7 @@ import { OPTION_LIST } from "@/app/common/constants";
 
 // `app/dashboard/page.tsx` is the UI for the `/dashboard` URL
 export default function FeatureGroupContainerComponent() {
-  const router = useRouter();
+
   let [selectedOption, setSelectedOption] = useState(OPTION_LIST);
   return <Row>
       <EntityMenu setSelectedOption={setSelectedOption} selectedOption={selectedOption} />
