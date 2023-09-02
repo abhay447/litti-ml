f0c9d9f7572d3b3e93776b4ff3dba4ef0ddcce77
abhay447 <abhay447@gmail.com>
2023-09-02 13:28:43 +0530

    propagate responses

--- litti-inference-router/internal/server/server.go
+++ litti-inference-router/internal/server/server.go
@@ -20,19 +20,27 @@ func getRoot(w http.ResponseWriter, r *http.Request) {
 	io.WriteString(w, "This is my website!\n")
 }
 
-func predict(w http.ResponseWriter, r *http.Request) {
+func validatePath(r *http.Request) (string, string, error) {
 	var tokens []string = strings.Split(r.URL.Path, "/")[1:]
 	fmt.Printf("Tokens: %+v", tokens)
+
 	if len(tokens) != 3 {
-		http.Error(w, "invalid url spec, use <base_url>/predict/<model_name>/<model_version>", http.StatusBadRequest)
+		return "", "", errors.New("invalid url spec, use <base_url>/predict/<model_name>/<model_version>")
+	}
+	return tokens[1], tokens[2], nil
+}
+
+func predict(w http.ResponseWriter, r *http.Request) {
+	model, version, err := validatePath(r)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
-	var model = tokens[1]
-	var version = tokens[2]
 	// read request for validation
 	// TODO: add feature fetch using request body
+	reqBytes, _ := ioutil.ReadAll(r.Body)
 	var p dto.BatchPredictionRequest
-	err := json.NewDecoder(r.Body).Decode(&p)
+	err = json.Unmarshal(reqBytes, &p)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
@@ -45,27 +53,29 @@ func predict(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	fmt.Printf("got /predict request for model: %s version: %s\n", model, version)
-	var reqBytes []byte
-	r.Body.Read(reqBytes)
-	var reqBodyReader = bytes.NewReader(reqBytes)
 	requestURL := fmt.Sprintf("http://localhost:8001/predict/%s/%s", model, version)
-	req, err := http.NewRequest(http.MethodPost, requestURL, reqBodyReader)
+	req, err := http.NewRequest(http.MethodPost, requestURL, bytes.NewReader(reqBytes))
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
+
 	client := &http.Client{}
 	res, err := client.Do(req)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-	if res.StatusCode != 200 {
-		bytedata, _ := ioutil.ReadAll(res.Body)
-		http.Error(w, string(bytedata), http.StatusInternalServerError)
+	resBodyBytes, _ := ioutil.ReadAll(res.Body)
+	resBody := string(resBodyBytes)
+	resStatus := res.StatusCode
+	if resStatus != 200 {
+		http.Error(w, resBody, http.StatusInternalServerError)
 		return
 	}
-	fmt.Fprintf(w, "Response: %+v", res)
+	w.WriteHeader(resStatus)
+	w.Write(resBodyBytes)
+	fmt.Fprintf(w, "Response: %+v", resBody)
 	// io.WriteString(w, "Hello, HTTP!\n")
 }
 
