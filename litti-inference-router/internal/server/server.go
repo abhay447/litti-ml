fe5bd6b95bff335a955927b3414a89803d196da8
abhay447 <abhay447@gmail.com>
2023-09-02 13:01:35 +0530

    propagate inference errors

--- litti-inference-router/internal/server/server.go
+++ litti-inference-router/internal/server/server.go
@@ -1,11 +1,13 @@
 package server
 
 import (
+	"bytes"
 	"com/litti/ml/litti-inference-router/internal/dto"
 	"encoding/json"
 	"errors"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"net/http"
 	"os"
 	"strings"
@@ -17,10 +19,6 @@ func getRoot(w http.ResponseWriter, r *http.Request) {
 	fmt.Printf("got / request\n")
 	io.WriteString(w, "This is my website!\n")
 }
-func getHello(w http.ResponseWriter, r *http.Request) {
-	fmt.Printf("got /hello request\n")
-	io.WriteString(w, "Hello, HTTP!\n")
-}
 
 func predict(w http.ResponseWriter, r *http.Request) {
 	var tokens []string = strings.Split(r.URL.Path, "/")[1:]
@@ -31,6 +29,8 @@ func predict(w http.ResponseWriter, r *http.Request) {
 	}
 	var model = tokens[1]
 	var version = tokens[2]
+	// read request for validation
+	// TODO: add feature fetch using request body
 	var p dto.BatchPredictionRequest
 	err := json.NewDecoder(r.Body).Decode(&p)
 	if err != nil {
@@ -45,7 +45,27 @@ func predict(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	fmt.Printf("got /predict request for model: %s version: %s\n", model, version)
-	fmt.Fprintf(w, "Request: %+v", p)
+	var reqBytes []byte
+	r.Body.Read(reqBytes)
+	var reqBodyReader = bytes.NewReader(reqBytes)
+	requestURL := fmt.Sprintf("http://localhost:8001/predict/%s/%s", model, version)
+	req, err := http.NewRequest(http.MethodPost, requestURL, reqBodyReader)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	client := &http.Client{}
+	res, err := client.Do(req)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	if res.StatusCode != 200 {
+		bytedata, _ := ioutil.ReadAll(res.Body)
+		http.Error(w, string(bytedata), http.StatusInternalServerError)
+		return
+	}
+	fmt.Fprintf(w, "Response: %+v", res)
 	// io.WriteString(w, "Hello, HTTP!\n")
 }
 
