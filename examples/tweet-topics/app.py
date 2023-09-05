2f2260f90dfcd168b068b810caccf0044f98757c
abhay447 <abhay447@gmail.com>
2023-09-06 00:21:50 +0530

    start server in app.py

--- examples/tweet-topics/app.py
+++ examples/tweet-topics/app.py
@@ -2,6 +2,7 @@ from flask import Flask, request
 import json
 import dto
 import tweetnlp # type: ignore
+import os
 
 app = Flask(__name__)
 model:tweetnlp.TopicClassification = tweetnlp.load_model('topic_classification',multi_label=False) # type: ignore
@@ -27,3 +28,6 @@ def predict(modelName:str,modelVersion:str):
     )
 
     return json.dumps(batchPredictRes,default=lambda o: o.encode())
+
+port = int(os.environ.get('PORT', 5000))
+app.run(debug=True, host='0.0.0.0', port=port)
